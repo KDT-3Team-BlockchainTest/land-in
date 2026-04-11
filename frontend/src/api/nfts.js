4e356a3200d96d41744f075bb6d9a8505a0f6045
tmdrb0130 <dltmdrb310@naver.com>
2026-04-11 10:56:50 +0900

    improve tag flow state refresh and ios redirect handling

--- frontend/src/api/nfts.js
+++ frontend/src/api/nfts.js
@@ -3,4 +3,6 @@ import { api } from './client';
 export const nftsApi = {
   /** GET /api/nfts?eventId=xxx */
   list: (eventId) => api.get(`/nfts${eventId ? `?eventId=${eventId}` : ''}`),
+  /** GET /api/nfts/:nftId */
+  getById: (nftId) => api.get(`/nfts/${nftId}`),
 };
